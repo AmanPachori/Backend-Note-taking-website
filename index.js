966a2fbb8aa6a6bdb8e3fd526be32bcb74ce8561
Aman Pachori <84467090+AmanPachori@users.noreply.github.com>
2023-11-22 16:14:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Change * to the specific origin if needed
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+
 const PORT = process.env.PORT || 5000;
 
 client.connect((err) => {
